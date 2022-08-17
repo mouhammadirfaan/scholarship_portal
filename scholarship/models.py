391ffad69e2315d8618568e647a39ab56c6f6dbd
MUHAMMAD IRFAN <mouhammadirfaan@gmail.com>
2022-08-17 13:15:50 +0500

    add

--- scholarship/models.py
+++ scholarship/models.py
@@ -1,3 +1,25 @@
+from pyexpat import model
+from re import T
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Create your All models here.
+
+# Student User start
+
+class StudentUser(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    mobile = models.CharField(max_length=12, null=False)
+    image = models.FileField(null=True)
+    gender = models.CharField(max_length=10, null=False)
+    usertype = models.CharField(max_length=30, null=False)
+
+    def __str__(self):
+        return self.user.username
+
+
+    
+
+
+# Student User end
+
