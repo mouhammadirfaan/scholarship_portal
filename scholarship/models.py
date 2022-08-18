f68b9c6d074dbaef8afc5b8fe905e2cb3776e14b
MUHAMMAD IRFAN <mouhammadirfaan@gmail.com>
2022-08-18 23:14:26 +0500

    add

--- scholarship/models.py
+++ scholarship/models.py
@@ -4,17 +4,31 @@ from django.contrib.auth.models import User
 # Create your All models here.
 
 # Student User start
-
 class StudentUser(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    mobile = models.CharField(max_length=12, null=False, default='0000000')
-    image = models.FileField(null=False)
+    mobile = models.CharField(max_length=20, null=True)
+    image = models.FileField()
     gender = models.CharField(max_length=10, null=False)
     usertype = models.CharField(max_length=30, null=False)
 
     def __str__(self):
         return self.user.username
 
+# Student User end
+
+# Provider User start
+class Provider(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    mobile = models.CharField(max_length=12, null=False)
+    image = models.FileField()
+    gender = models.CharField(max_length=10, null=False)
+    companyname = models.CharField(max_length=50, null=False)
+    usertype = models.CharField(max_length=30, null=False)
+    status = models.CharField(max_length=30, null=False)
+
+    def __str__(self):
+        return self.user.username
+# Provider User end
 
     
 
