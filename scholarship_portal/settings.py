c033a143a61ba7ceb70cea81f0cbf5d0f40982b2
MUHAMMAD IRFAN <mouhammadirfaan@gmail.com>
2022-09-19 20:12:31 +0500

    add

--- scholarship_portal/settings.py
+++ scholarship_portal/settings.py
@@ -143,16 +143,16 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = '/login/'
 
-# Forget Password
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 587
-# EMAIL_HOST_USER = 'mouhammadirfaan@gmail.com'
-# EMAIL_HOST_PASSWORD = 'password'
-# EMAIL_USE_TLS = True
+#Forget Password
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'mouhammadirfaan@gmail.com'
+EMAIL_HOST_PASSWORD = 'xgkhllwbvwlcnost'   
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 
 
