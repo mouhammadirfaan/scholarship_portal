0b00fd3b8be92df67f7e9ac23bbbdd112ef5f174
MUHAMMAD IRFAN <mouhammadirfaan@gmail.com>
2022-09-20 07:30:39 +0500

    add

--- scholarship_portal/settings.py
+++ scholarship_portal/settings.py
@@ -149,7 +149,9 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'mouhammadirfaan@gmail.com'
-EMAIL_HOST_PASSWORD = 'xgkhllwbvwlcnost'   
+EMAIL_HOST_PASSWORD = 'knulqecybstehrjp' 
+
+
 
 
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
